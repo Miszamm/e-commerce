ac5472c1d5b9d1af63fc1b3e025c1490c3d5bfe0
Michal Misza <miszamm@yahoo.ie>
2021-10-12 17:21:54 +0000

    fixed Styling issues in profile page

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -125,10 +125,9 @@ WSGI_APPLICATION = 'e_commerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 
 # Password validation
